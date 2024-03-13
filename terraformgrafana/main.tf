f972977580bbcbd07df1835bc3838207c89802b1
iancumatei67 <127155074+iancumatei67@users.noreply.github.com>
2024-03-13 12:19:20 +0200

    Terraform automation for data_source and dashboard

---
+++ terraformgrafana/main.tf
@@ -0,0 +1,31 @@
+terraform {
+    required_providers {
+        grafana = {
+            source = "grafana/grafana"
+            version = "~> 1.13.0"
+        }
+    }
+}
+
+
+provider "grafana" {
+    url = "http://172.20.103.62:3000"
+    auth = "glsa_lZUK7O3ZsDuUBEffU84id8frhH7xGnAK_06ec5fda"
+}
+
+resource "grafana_data_source" "prometheus" {
+  name        = "Prometheus"
+  type        = "prometheus"
+  url         = "http://172.20.103.62:9090"  
+  is_default  = true
+}
+
+resource "grafana_dashboard" "example_dashboard" {
+  depends_on = [grafana_data_source.prometheus]
+  config_json = jsonencode({
+    "title" : "Dashboard from Terraform",
+    "uid" : "terraformdashboard"
+
+  })
+
+}
